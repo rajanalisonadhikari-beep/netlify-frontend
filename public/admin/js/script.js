3db2c47c5c3ae3a96ed0c3d165666e0516f0f2d1
rajanalisonadhikari-beep <rajanalisonadhikari@gmail.com>
2026-08-02 13:39:47 +0545

    Delete public directory

--- public/admin/js/script.js
+++
@@ -1,8 +0,0 @@
-const sidebar = document.getElementById('sidebar');
-const sidebarToggle = document.getElementById('sidebarToggle');
-
-if (sidebarToggle && sidebar) {
-  sidebarToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('collapsed');
-  });
-}
